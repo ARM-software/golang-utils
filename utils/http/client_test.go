1251240085e67461b02aa06725bc81dd5010e8e7
Alejandro Germain <alejandro.germain@arm.com>
2020-11-06 13:37:21 +0000

    Address comments and refactor tests

--- utils/http/client_test.go
+++ utils/http/client_test.go
@@ -1,26 +1,39 @@
 package http
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
+
+func createTestServer(t *testing.T, handler http.Handler, port string, ch chan error) {
+	// Create a test server
+	list, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	require.Nil(t, err)
+
+	go func() {
+		defer list.Close()
+		err = http.Serve(list, handler)
+		if err != nil {
+			ch <- err
+		}
+	}()
+}
 
 // This test will sleep for 50ms after the request is made asynchronously via RetryableClient
 // as to test that the exponential backoff is working.
 func TestClient_Delete_Backoff(t *testing.T) {
 	// Collect errors for goroutines
 	errs := make(chan error)
-
 	// Mock server which always responds 200.
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "DELETE" {
-			t.Fatalf("bad method: %s", r.Method)
-		}
-		if r.RequestURI != "/foo/bar" {
-			t.Fatalf("bad uri: %s", r.RequestURI)
-		}
+		require.Equal(t, r.Method, "DELETE")
+		require.Equal(t, r.RequestURI, "/foo/bar")
+		// The request succeeds
 		w.WriteHeader(200)
 	})
 
@@ -32,31 +45,20 @@ func TestClient_Delete_Backoff(t *testing.T) {
 		resp, err := NewRetryableClient().Delete("http://127.0.0.1:28934/foo/bar")
 		if err != nil {
 			errs <- err
+		} else {
+			resp.Body.Close()
 		}
-		resp.Body.Close()
 	}()
-	time.Sleep(50 * time.Millisecond)
 
-	// Create a test server
-	list, err := net.Listen("tcp", ":28934")
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	go func() {
-		defer list.Close()
-		err = http.Serve(list, handler)
-		if err != nil {
-			errs <- err
-		}
-	}()
+	// We set this to simulate a first error on the first request so that we can properly test the backoff
+	time.Sleep(50 * time.Millisecond)
+	createTestServer(t, handler, "28934", errs)
 
 	<-doneCh
 	// close errs once all children finish.
 	close(errs)
 	for err := range errs {
-		if err != nil {
-			t.Fatalf("err: %v", err)
-		}
+		require.Nil(t, err)
 	}
 }
 
@@ -68,28 +70,13 @@ func TestClient_Get_Fail_Timeout(t *testing.T) {
 
 	// Mock server which always responds 200.
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			t.Fatalf("bad method: %s", r.Method)
-		}
-		if r.RequestURI != "/foo/bar" {
-			t.Fatalf("bad uri: %s", r.RequestURI)
-		}
+		require.Equal(t, r.Method, "GET")
+		require.Equal(t, r.RequestURI, "/foo/bar")
+		// The request fails
 		w.WriteHeader(404)
 	})
 
-	// Create a test server
-	list, err := net.Listen("tcp", ":28935")
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-
-	go func() {
-		defer list.Close()
-		err = http.Serve(list, handler)
-		if err != nil {
-			errs <- err
-		}
-	}()
+	createTestServer(t, handler, "28935", errs)
 
 	// Send the asynchronous request
 	var resp *http.Response
@@ -100,17 +87,16 @@ func TestClient_Get_Fail_Timeout(t *testing.T) {
 		resp, err = NewRetryableClient().Get("http://127.0.0.1:28935/foo/bar")
 		if err != nil {
 			errs <- err
+		} else {
+			resp.Body.Close()
 		}
-		resp.Body.Close()
 	}()
 	select {
 	case <-doneCh:
 		// close errs once all children finish.
 		close(errs)
 		for err := range errs {
-			if err != nil {
-				t.Fatalf("err: %v", err)
-			}
+			require.Nil(t, err)
 		}
 	case <-time.After(1000 * time.Millisecond):
 		t.Fatalf("Client should fail instantly with a 404 response, not retrying!")
