e890c30956d243440c4eb11afc687d72bc60e1a7
Josh <josh.jennings@arm.com>
2022-05-12 17:16:19 +0100

    :sparkles: [collection] `ParseListWithCleanupKeepBlankLines` as alternative to `ParseListWithCleanup` to keep blank lines (#82)
    Add option to run `ParseListWithCleanupKeepBlankLines` to run `ParseListWithCleanup` but keep empty lines. This is necessary for the VHT Service. Backwards compatible with old versions.

--- utils/collection/parseLists.go
+++ utils/collection/parseLists.go
@@ -4,12 +4,21 @@
  */
 package collection
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
-// ParseListWithCleanup splits a string into a list like strings.Split but also removes any whitespace surrounding  the different items
-// for example,
-// ParseListWithCleanup("a, b ,  c", ",") returns []{"a","b","c"}
-func ParseListWithCleanup(input string, sep string) (newS []string) {
+func lineIsOnlyWhitespace(line string) bool {
+	for _, c := range line {
+		if !unicode.IsSpace(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func parseListWithCleanup(input string, sep string, keepBlankLines bool) (newS []string) {
 	if len(input) == 0 {
 		newS = []string{} // initialisation of empty arrays in function returns []string(nil) instead of []string{}
 		return
@@ -17,11 +26,26 @@ func ParseListWithCleanup(input string, sep string) (newS []string) {
 	split := strings.Split(input, sep)
 	for _, s := range split {
 		tempString := strings.TrimSpace(s)
-		if tempString != "" {
+		if tempString != "" || (keepBlankLines && lineIsOnlyWhitespace(s)) {
 			newS = append(newS, tempString)
 		}
 	}
 	return
+}
+
+// ParseListWithCleanup splits a string into a list like strings.Split but also removes any whitespace surrounding  the different items
+// for example,
+// ParseListWithCleanup("a, b ,  c", ",") returns []{"a","b","c"}
+func ParseListWithCleanup(input string, sep string) (newS []string) {
+	return parseListWithCleanup(input, sep, false)
+}
+
+// ParseListWithCleanupKeepBlankLines splits a string into a list like strings.Split but also removes any whitespace surrounding  the different items
+// unless the entire item is whitespace in which case it is converted to an empty string. For example,
+// ParseListWithCleanupKeepBlankLines("a, b ,  c", ",") returns []{"a","b","c"}
+// ParseListWithCleanupKeepBlankLines("a, b ,    , c", ",") returns []{"a","b", "", "c"}
+func ParseListWithCleanupKeepBlankLines(input string, sep string) (newS []string) {
+	return parseListWithCleanup(input, sep, true)
 }
 
 // ParseCommaSeparatedList returns the list of string separated by a comma
