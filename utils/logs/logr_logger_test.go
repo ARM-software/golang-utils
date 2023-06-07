3892db78eb883ee9588e91c0495fedde2571c9a0
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2023-06-07 14:06:41 +0100

    :sparkles: `logs`Add GetLogrLoggerFromContext (#272)
    <!--
    Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors.
    All rights reserved.
    SPDX-License-Identifier: Apache-2.0
    -->
    ### Description
    
    - `logs`Add GetLogrLoggerFromContext to fetch a logr.logger from a
    context (#271)
    
    ### Test Coverage
    
    <!--
    Please put an `x` in the correct box e.g. `[x]` to indicate the testing
    coverage of this change.
    -->
    
    - [x]  This change is covered by existing or additional automated tests.
    - [ ] Manual testing has been performed (and evidence provided) as
    automated testing was not feasible.
    - [ ] Additional tests are not required for this change (e.g.
    documentation update).
    
    ---------
    
    <!--
    Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors.
    All rights reserved.
    SPDX-License-Identifier: Apache-2.0
    -->
    ### Description
    
    <!--
    Please add any detail or context that would be useful to a reviewer.
    -->
    
    
    
    ### Test Coverage
    
    <!--
    Please put an `x` in the correct box e.g. `[x]` to indicate the testing
    coverage of this change.
    -->
    
    - [ ]  This change is covered by existing or additional automated tests.
    - [ ] Manual testing has been performed (and evidence provided) as
    automated testing was not feasible.
    - [ ] Additional tests are not required for this change (e.g.
    documentation update).
    
    Co-authored-by: Luke Parkin <65398091+Luke-Parkin@users.noreply.github.com>

--- utils/logs/logr_logger_test.go
+++ utils/logs/logr_logger_test.go
@@ -5,12 +5,16 @@
 package logs
 
 import (
+	"context"
 	"testing"
 
 	"github.com/bxcodec/faker/v3"
+	"github.com/go-logr/logr"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ARM-software/golang-utils/utils/commonerrors"
+	"github.com/ARM-software/golang-utils/utils/commonerrors/errortest"
 	"github.com/ARM-software/golang-utils/utils/logs/logstest"
 )
 
@@ -29,4 +33,23 @@ func TestLogrLoggerConversion(t *testing.T) {
 	require.NoError(t, err)
 	converted := NewLogrLoggerFromLoggers(loggers)
 	converted.WithName(faker.Name()).WithValues(faker.Word(), faker.Name()).Error(commonerrors.ErrUnexpected, faker.Sentence())
+}
+
+func TestGetLogrFromEmptyContext(t *testing.T) {
+	ctx := context.Background()
+	logger, err := GetLogrLoggerFromContext(ctx)
+
+	assert.Equal(t, logr.Logger{}, logger)
+	errortest.AssertError(t, err, commonerrors.ErrNoLogger)
+}
+
+func TestGetLogrFromContext(t *testing.T) {
+	ctx := context.Background()
+	logger := logstest.NewTestLogger(t)
+	ctx = logr.NewContext(ctx, logger)
+
+	newLogger, err := GetLogrLoggerFromContext(ctx)
+	assert.NoError(t, err)
+
+	assert.Equal(t, logger, newLogger)
 }
