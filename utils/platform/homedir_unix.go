e4e2f7c3de4f6ce31a58def8825afdb4704b0c70
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2023-10-26 19:53:36 +0100

    linting

--- utils/platform/homedir_unix.go
+++ utils/platform/homedir_unix.go
@@ -15,7 +15,7 @@ func determineDefaultHomeDirectory(username string) (string, error) {
 	currentDir, subErr1 := homedir.Dir()
 	currentUser, subErr2 := user.Current()
 	if subErr1 != nil || subErr2 != nil {
-		return filepath.Join(`/`, "home", username)
+		return filepath.Join(`/`, "home", username), nil
 	}
 	return strings.ReplaceAll(currentDir, currentUser.Username, username), nil
 }
