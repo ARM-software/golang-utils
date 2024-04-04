703e8c85dbb4582672dd66aa871c6f0f847cd855
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2024-04-04 09:21:43 +0100

    Update utils/subprocess/command_wrapper_windows.go

--- utils/subprocess/command_wrapper_windows.go
+++ utils/subprocess/command_wrapper_windows.go
@@ -10,7 +10,8 @@ import (
 
 func setGroupAttrToCmd(c *exec.Cmd) {
 	c.SysProcAttr = &syscall.SysProcAttr{
-		HideWindow:    true,
+		HideWindow: true,
+		// Windows Process Creation Flags:  https://learn.microsoft.com/en-us/windows/win32/procthread/process-creation-flags
 		CreationFlags: syscall.CREATE_UNICODE_ENVIRONMENT | syscall.CREATE_NEW_PROCESS_GROUP,
 	}
 }
