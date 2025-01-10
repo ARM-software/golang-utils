545d07c65fe78ec65e4237bf2f96365456f4ffd5
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2025-01-10 14:31:10 +0000

    :sparkles: `[collection]` add helpers easier to use with slices (#547)
    <!--
    Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors.
    All rights reserved.
    SPDX-License-Identifier: Apache-2.0
    -->
    ### Description
    
    add utilities to the collection module
    
    
    ### Test Coverage
    
    <!--
    Please put an `x` in the correct box e.g. `[x]` to indicate the testing
    coverage of this change.
    -->
    
    - [x]  This change is covered by existing or additional automated tests.
    - [ ] Manual testing has been performed (and evidence provided) as
    automated testing was not feasible.
    - [ ] Additional tests are not required for this change (e.g.
    documentation update).

--- utils/collection/search.go
+++ utils/collection/search.go
@@ -59,12 +59,30 @@ func UniqueEntries[T comparable](slice []T) []T {
 
 // Any returns true if there is at least one element of the slice which is true.
 func Any(slice []bool) bool {
+	if len(slice) == 0 {
+		return false
+	}
 	for i := range slice {
 		if slice[i] {
 			return true
 		}
 	}
 	return false
+}
+
+// AnyTrue returns whether there is a value set to true
+func AnyTrue(values ...bool) bool {
+	return Any(values)
+}
+
+// AnyFunc returns whether there is at least one element of slice s for which f() returns true.
+func AnyFunc[S ~[]E, E any](s S, f func(E) bool) bool {
+	all := make([]bool, 0, len(s))
+	for i := range s {
+		all = append(all, f(s[i]))
+
+	}
+	return Any(all)
 }
 
 // AnyEmpty returns whether there is one entry in the slice which is empty.
@@ -76,12 +94,30 @@ func AnyEmpty(strict bool, slice []string) bool {
 
 // All returns true if all items of the slice are true.
 func All(slice []bool) bool {
+	if len(slice) == 0 {
+		return false
+	}
 	for i := range slice {
 		if !slice[i] {
 			return false
 		}
 	}
 	return true
+}
+
+// AllTrue returns whether all values are true.
+func AllTrue(values ...bool) bool {
+	return All(values)
+}
+
+// AllFunc returns whether f returns true for all the elements of slice s.
+func AllFunc[S ~[]E, E any](s S, f func(E) bool) bool {
+	all := make([]bool, 0, len(s))
+	for i := range s {
+		all = append(all, f(s[i]))
+
+	}
+	return All(all)
 }
 
 // AllNotEmpty returns whether all elements of the slice are not empty.
