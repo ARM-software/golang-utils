910f3080aeca8b715c6076ff58cbfac7d8a134ff
joshjennings98 <josh.jennings@arm.com>
2025-02-11 11:57:13 +0000

    lint

--- utils/subprocess/supervisor/interface.go
+++ utils/subprocess/supervisor/interface.go
@@ -7,6 +7,6 @@ import "context"
 // ISupervisor  will run a command and automatically restart it if it exits. Hooks can be used to execute code at
 // different points in the execution lifecyle. Restarts can be delayed
 type ISupervisor interface {
-       // Run will run the supervisor and execute any of the command hooks. If it receives a halting error or the context is cancelled then it will exit
+	// Run will run the supervisor and execute any of the command hooks. If it receives a halting error or the context is cancelled then it will exit
 	Run(ctx context.Context) error
 }
