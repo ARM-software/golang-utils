87221be0653d6baf783b4590edfd6fec17d29555
joshjennings98 <josh.jennings@arm.com>
2025-03-28 14:18:06 +0000

    commonerrors

--- utils/encryption/aesrsa/encryption.go
+++ utils/encryption/aesrsa/encryption.go
@@ -8,7 +8,6 @@ import (
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
 
 	"github.com/ARM-software/golang-utils/utils/commonerrors"
 	"github.com/ARM-software/golang-utils/utils/filesystem"
@@ -17,24 +16,24 @@ import (
 // ParsePEMBlock will parse the first PEM block found within path
 func ParsePEMBlock(path string) (block *pem.Block, err error) {
 	if path == "" {
-		err = fmt.Errorf("%w: no certificate provided", commonerrors.ErrUndefined)
+		err = commonerrors.New(commonerrors.ErrUndefined, "no certificate provided")
 		return
 	}
 
 	if !filesystem.Exists(path) {
-		err = fmt.Errorf("%w: could not find certificate at '%v'", commonerrors.ErrNotFound, path)
+		err = commonerrors.Newf(commonerrors.ErrNotFound, "could not find certificate at '%v'", path)
 		return
 	}
 
 	certBytes, err := filesystem.ReadFile(path)
 	if err != nil {
-		err = fmt.Errorf("%w: failed to read certificate: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "failed to read certificate")
 		return
 	}
 
 	block, _ = pem.Decode(certBytes)
 	if block == nil {
-		err = fmt.Errorf("%w: failed to decode PEM block from certificate", commonerrors.ErrUnexpected)
+		err = commonerrors.New(commonerrors.ErrUnexpected, "failed to decode PEM block from certificate")
 		return
 	}
 
@@ -47,60 +46,60 @@ func ParsePEMBlock(path string) (block *pem.Block, err error) {
 func DecryptHybridAESRSAEncryptedPayloadFromPrivateKey(privateKeyPath string, payload *HybridAESRSAEncryptedPayload) (decrypted []byte, err error) {
 	block, err := ParsePEMBlock(privateKeyPath)
 	if err != nil {
-		err = fmt.Errorf("%w: could not parse PEM block from '%v': %v", commonerrors.ErrUnexpected, privateKeyPath, err.Error())
+		err = commonerrors.WrapErrorf(commonerrors.ErrUnexpected, err, "could not parse PEM block from '%v'", privateKeyPath)
 		return
 	}
 
 	if block == nil {
-		err = fmt.Errorf("%w: block was empty", commonerrors.ErrEmpty)
+		err = commonerrors.New(commonerrors.ErrEmpty, "block was empty")
 		return
 	}
 
 	return DecryptHybridAESRSAEncryptedPayloadFromBytes(block.Bytes, payload)
 }
 
-// DecryptHybridAESRSAEncryptedPayloadFromPrivateKeyPath takes a path to an RSA private key PEM file and uses it to
+// DecryptHybridAESRSAEncryptedPayloadFromBytes takes a path to an RSA private key PEM file and uses it to
 // decode the AES key in a hybrid encoded payload. This AES key is then used to decode the actual payload contents.
 // Information of the use of hybrid AES RSA encryption can be found here https://www.ijrar.org/papers/IJRAR23B1852.pdf
 func DecryptHybridAESRSAEncryptedPayloadFromBytes(block []byte, payload *HybridAESRSAEncryptedPayload) (decrypted []byte, err error) {
 	if payload == nil {
-		err = fmt.Errorf("%w: payload must not be nil", commonerrors.ErrUndefined)
+		err = commonerrors.New(commonerrors.ErrUndefined, "payload must not be nil")
 		return
 	}
 
 	priv, err := x509.ParsePKCS1PrivateKey(block)
 	if err != nil {
-		err = fmt.Errorf("%w: could not parse private key %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "could not parse private key")
 		return
 	}
 
 	ciphertext, encryptedKey, nonce, err := DecodeHybridAESRSAEncryptedPayload(payload)
 	if err != nil {
-		err = fmt.Errorf("%w: could not decode payload: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "could not decode payload")
 		return
 	}
 
 	aesKey, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, priv, encryptedKey, []byte{})
 	if err != nil {
-		err = fmt.Errorf("%w: could not decrypt private key %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "could not decrypt private key")
 		return
 	}
 
 	blockCipher, err := aes.NewCipher(aesKey)
 	if err != nil {
-		err = fmt.Errorf("%w: could not create new cipher %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "could not create new cipher")
 		return
 	}
 
 	aesGCM, err := cipher.NewGCM(blockCipher)
 	if err != nil {
-		err = fmt.Errorf("%w: could not create new GCM %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "could not create new GCM")
 		return
 	}
 
 	decrypted, err = aesGCM.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		err = fmt.Errorf("%w: could not open ciphertext %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "could not open ciphertext")
 		return
 	}
 
@@ -109,40 +108,40 @@ func DecryptHybridAESRSAEncryptedPayloadFromBytes(block []byte, payload *HybridA
 
 func encryptWithRSAKey(rsaPub *rsa.PublicKey, payload []byte) (encrypted *HybridAESRSAEncryptedPayload, err error) {
 	if rsaPub == nil {
-		err = fmt.Errorf("%w: rsa public key is undefined", commonerrors.ErrUndefined)
+		err = commonerrors.New(commonerrors.ErrUndefined, "rsa public key is undefined")
 		return
 	}
 
 	aesKey := make([]byte, 32)
 	_, err = rand.Read(aesKey)
 	if err != nil {
-		err = fmt.Errorf("%w: failed generating AES key: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "failed generating AES key")
 		return
 	}
 
 	blockCipher, err := aes.NewCipher(aesKey)
 	if err != nil {
-		err = fmt.Errorf("%w: failed to create cipher: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "failed to create cipher")
 		return
 	}
 
 	aesGCM, err := cipher.NewGCM(blockCipher)
 	if err != nil {
-		err = fmt.Errorf("%w: failed creating GCM: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "failed creating GCM")
 		return
 	}
 
 	nonce := make([]byte, aesGCM.NonceSize())
 	_, err = rand.Read(nonce)
 	if err != nil {
-		err = fmt.Errorf("%w: failed to generate nonce: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "failed to generate nonce")
 		return
 	}
 
 	ciphertext := aesGCM.Seal(nil, nonce, payload, nil)
 	encryptedAESKey, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, rsaPub, aesKey, []byte{})
 	if err != nil {
-		err = fmt.Errorf("%w: could not complete RSA encryption: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "could not complete RSA encryption")
 		return
 	}
 
@@ -158,12 +157,12 @@ func encryptWithRSAKey(rsaPub *rsa.PublicKey, payload []byte) (encrypted *Hybrid
 func EncryptHybridAESRSAEncryptedPayloadFromPublickKeyBytes(publicKeyBytes []byte, payload []byte) (encrypted *HybridAESRSAEncryptedPayload, err error) {
 	block, _ := pem.Decode(publicKeyBytes)
 	if block == nil {
-		err = fmt.Errorf("%w: failed to decode PEM block from certificate", commonerrors.ErrUnexpected)
+		err = commonerrors.New(commonerrors.ErrUnexpected, "failed to decode PEM block from certificate")
 		return
 	}
 
 	if block == nil {
-		err = fmt.Errorf("%w: block was empty", commonerrors.ErrEmpty)
+		err = commonerrors.New(commonerrors.ErrEmpty, "block was empty")
 		return
 	}
 
@@ -171,7 +170,7 @@ func EncryptHybridAESRSAEncryptedPayloadFromPublickKeyBytes(publicKeyBytes []byt
 	if err != nil {
 		pkcs1PublicKey, subErr := x509.ParsePKCS1PublicKey(block.Bytes)
 		if subErr != nil {
-			err = fmt.Errorf("failed to parse public key as PKIX or PKCS1: %v, %v", err, subErr)
+			err = commonerrors.Newf(commonerrors.ErrUnexpected, "failed to parse public key as PKIX or PKCS1: %v, %v", err, subErr)
 			return
 		}
 
@@ -180,7 +179,7 @@ func EncryptHybridAESRSAEncryptedPayloadFromPublickKeyBytes(publicKeyBytes []byt
 
 	rsaPub, ok := pub.(*rsa.PublicKey)
 	if !ok {
-		err = fmt.Errorf("%w: could not parse PKIX pem block as RSA key", commonerrors.ErrUnexpected)
+		err = commonerrors.New(commonerrors.ErrUnexpected, "could not parse PKIX pem block as RSA key")
 		return
 	}
 
@@ -195,13 +194,13 @@ func EncryptHybridAESRSAEncryptedPayloadFromPublickKeyBytes(publicKeyBytes []byt
 func EncryptHybridAESRSAEncryptedPayloadFromBytes(block []byte, payload []byte) (encrypted *HybridAESRSAEncryptedPayload, err error) {
 	cert, err := x509.ParseCertificate(block)
 	if err != nil {
-		err = fmt.Errorf("%w: failed parsing certificate: %v", commonerrors.ErrUnexpected, err.Error())
+		err = commonerrors.WrapError(commonerrors.ErrUnexpected, err, "failed parsing certificate")
 		return
 	}
 
 	rsaPub, ok := cert.PublicKey.(*rsa.PublicKey)
 	if !ok {
-		err = fmt.Errorf("%w: public key in certificate is not RSA", commonerrors.ErrInvalid)
+		err = commonerrors.New(commonerrors.ErrInvalid, "public key in certificate is not RSA")
 		return
 	}
 
@@ -216,12 +215,12 @@ func EncryptHybridAESRSAEncryptedPayloadFromBytes(block []byte, payload []byte) 
 func EncryptHybridAESRSAEncryptedPayloadFromCertificate(certPath string, payload []byte) (encrypted *HybridAESRSAEncryptedPayload, err error) {
 	block, err := ParsePEMBlock(certPath)
 	if err != nil {
-		err = fmt.Errorf("%w: could not parse PEM block from '%v': %v", commonerrors.ErrUnexpected, certPath, err.Error())
+		err = commonerrors.WrapErrorf(commonerrors.ErrUnexpected, err, "could not parse PEM block from '%v'", certPath)
 		return
 	}
 
 	if block == nil {
-		err = fmt.Errorf("%w: block was empty", commonerrors.ErrEmpty)
+		err = commonerrors.New(commonerrors.ErrEmpty, "block was empty")
 		return
 	}
 
