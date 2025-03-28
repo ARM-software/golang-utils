1b9a24e9fbf12298804951d9683a96a2f1e171fa
joshjennings98 <josh.jennings@arm.com>
2025-03-28 13:50:49 +0000

    :sparkles: `encruption` Add utility to encrypt payloads using public RSA key directly instead of using the certificate

---
+++ changes/20250328135041.feature
@@ -0,0 +1,1 @@
+:sparkles: `encruption` Add utility to encrypt payloads using public RSA key directly instead of using the certificate
--- utils/encryption/aesrsa/encryption.go
+++ utils/encryption/aesrsa/encryption.go
@@ -107,6 +107,82 @@ func DecryptHybridAESRSAEncryptedPayloadFromBytes(block []byte, payload *HybridA
 	return
 }
 
+func encrypteWithRSAKey(rsaPub *rsa.PublicKey, payload []byte) (encrypted *HybridAESRSAEncryptedPayload, err error) {
+	if rsaPub == nil {
+		err = fmt.Errorf("%w: rsa public key is undefined", commonerrors.ErrUndefined)
+		return
+	}
+
+	aesKey := make([]byte, 32)
+	_, err = rand.Read(aesKey)
+	if err != nil {
+		err = fmt.Errorf("%w: failed generating AES key: %v", commonerrors.ErrUnexpected, err.Error())
+		return
+	}
+
+	blockCipher, err := aes.NewCipher(aesKey)
+	if err != nil {
+		err = fmt.Errorf("%w: failed to create cipher: %v", commonerrors.ErrUnexpected, err.Error())
+		return
+	}
+	aesGCM, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		err = fmt.Errorf("%w: failed creating GCM: %v", commonerrors.ErrUnexpected, err.Error())
+		return
+	}
+	nonce := make([]byte, aesGCM.NonceSize())
+	_, err = rand.Read(nonce)
+	if err != nil {
+		err = fmt.Errorf("%w: failed to generate nonce: %v", commonerrors.ErrUnexpected, err.Error())
+		return
+	}
+	ciphertext := aesGCM.Seal(nil, nonce, payload, nil)
+	encryptedAESKey, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, rsaPub, aesKey, []byte{})
+	if err != nil {
+		err = fmt.Errorf("%w: could not complete RSA encryption: %v", commonerrors.ErrUnexpected, err.Error())
+		return
+	}
+	encrypted = EncodeHybridAESRSAEncryptedPayload(ciphertext, encryptedAESKey, nonce)
+	return
+}
+
+// EncryptHybridAESRSAEncryptedPayloadFromPublickKeyBytes takes a bublic key for key encypherment and uses it to
+// encode a payload using hybrid RSA AES encryption where an AES key is used to encrypt the content in payload and the
+// AES key is encrypted using RSA encryption. AES encryption is used to encode the payload itself as it is faster than
+// RSA for larger payloads. RSA is used to encrypt the relatively small AES key and allows asymmetric encryption
+// whilst also being fast. More information can be found at https://www.ijrar.org/papers/IJRAR23B1852.pdf
+func EncryptHybridAESRSAEncryptedPayloadFromPublickKeyBytes(publicKeyBytes []byte, payload []byte) (encrypted *HybridAESRSAEncryptedPayload, err error) {
+	block, _ := pem.Decode(publicKeyBytes)
+	if block == nil {
+		err = fmt.Errorf("%w: failed to decode PEM block from certificate", commonerrors.ErrUnexpected)
+		return
+	}
+
+	if block == nil {
+		err = fmt.Errorf("%w: block was empty", commonerrors.ErrEmpty)
+		return
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		pkcs1PublicKey, subErr := x509.ParsePKCS1PublicKey(block.Bytes)
+		if subErr != nil {
+			err = fmt.Errorf("failed to parse public key as PKIX or PKCS1: %v, %v", err, subErr)
+			return
+		}
+
+		return encrypteWithRSAKey(pkcs1PublicKey, payload)
+	}
+
+	rsaPub, ok := pub.(*rsa.PublicKey)
+	if !ok {
+		err = fmt.Errorf("%w: could not parse PKIX pem block as RSA key", commonerrors.ErrUnexpected)
+		return
+	}
+
+	return encrypteWithRSAKey(rsaPub, payload)
+}
+
 // EncryptHybridAESRSAEncryptedPayloadFromBytes takes an x509 certificate for key encypherment and uses it to
 // encode a payload using hybrid RSA AES encryption where an AES key is used to encrypt the content in payload and the
 // AES key is encrypted using RSA encryption. AES encryption is used to encode the payload itself as it is faster than
@@ -125,41 +201,7 @@ func EncryptHybridAESRSAEncryptedPayloadFromBytes(block []byte, payload []byte) 
 		return
 	}
 
-	aesKey := make([]byte, 32)
-	_, err = rand.Read(aesKey)
-	if err != nil {
-		err = fmt.Errorf("%w: failed generating AES key: %v", commonerrors.ErrUnexpected, err.Error())
-		return
-	}
-
-	blockCipher, err := aes.NewCipher(aesKey)
-	if err != nil {
-		err = fmt.Errorf("%w: failed to create cipher: %v", commonerrors.ErrUnexpected, err.Error())
-		return
-	}
-
-	aesGCM, err := cipher.NewGCM(blockCipher)
-	if err != nil {
-		err = fmt.Errorf("%w: failed creating GCM: %v", commonerrors.ErrUnexpected, err.Error())
-		return
-	}
-
-	nonce := make([]byte, aesGCM.NonceSize())
-	_, err = rand.Read(nonce)
-	if err != nil {
-		err = fmt.Errorf("%w: failed to generate nonce: %v", commonerrors.ErrUnexpected, err.Error())
-		return
-	}
-
-	ciphertext := aesGCM.Seal(nil, nonce, payload, nil)
-	encryptedAESKey, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, rsaPub, aesKey, []byte{})
-	if err != nil {
-		err = fmt.Errorf("%w: could not complete RSA encryption: %v", commonerrors.ErrUnexpected, err.Error())
-		return
-	}
-
-	encrypted = EncodeHybridAESRSAEncryptedPayload(ciphertext, encryptedAESKey, nonce)
-	return
+	return encrypteWithRSAKey(rsaPub, payload)
 }
 
 // EncryptHybridAESRSAEncryptedPayloadFromCertificate takes a path to a valid x509 certificate for key encypherment
