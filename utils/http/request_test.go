acafb7fc5d4887973d9d3d1e62fe42483941abda
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2025-05-12 18:32:39 +0100

    :sparkles: `[http]` Add a way to retrieve an http.Auth configuration from `Authorization` header values (#615)
    <!--
    Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors.
    All rights reserved.
    SPDX-License-Identifier: Apache-2.0
    -->
    ### Description
    - http module
    Add a constructor to create an Auth configuration from an Header string
    value
    
    
    ### Test Coverage
    
    <!--
    Please put an `x` in the correct box e.g. `[x]` to indicate the testing
    coverage of this change.
    -->
    
    - [x]  This change is covered by existing or additional automated tests.
    - [ ] Manual testing has been performed (and evidence provided) as
    automated testing was not feasible.
    - [ ] Additional tests are not required for this change (e.g.
    documentation update).

--- utils/http/request_test.go
+++ utils/http/request_test.go
@@ -4,7 +4,10 @@ import (
 	"testing"
 
 	"github.com/go-faker/faker/v4"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/ARM-software/golang-utils/utils/field"
 )
 
 func TestRequestConfiguration_Validate(t *testing.T) {
@@ -22,4 +25,29 @@ func TestRequestConfiguration_Validate(t *testing.T) {
 	require.Error(t, cfg.Validate())
 	cfg.Authorisation.Scheme = AuthorisationSchemeToken
 	require.NoError(t, cfg.Validate())
+}
+
+func TestAuth(t *testing.T) {
+	cfg, err := NewAuthConfiguration(nil)
+	require.NoError(t, err)
+	assert.False(t, cfg.Enforced)
+	cfg.Enforced = true
+	require.Error(t, cfg.Validate())
+	cfg.AccessToken = faker.Password()
+	cfg.Scheme = faker.Name()
+	require.Error(t, cfg.Validate())
+	cfg.Scheme = AuthorisationSchemeToken
+	require.NoError(t, cfg.Validate())
+	cfg2, err := NewAuthConfiguration(field.ToOptionalString(faker.Password()))
+	require.Error(t, err)
+	assert.Empty(t, cfg2)
+	cfg2, err = NewAuthConfiguration(field.ToOptionalString(cfg.GetAuthorizationHeader()))
+	require.NoError(t, err)
+	require.NoError(t, cfg2.Validate())
+	assert.True(t, cfg.Enforced)
+	cfg.Scheme = faker.Name()
+	cfg2, err = NewAuthConfiguration(field.ToOptionalString(cfg.GetAuthorizationHeader()))
+	require.Error(t, err)
+	require.Error(t, cfg2.Validate())
+	assert.True(t, cfg.Enforced)
 }
