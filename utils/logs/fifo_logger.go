5771cc3bad0c24235018d9dbaf48082b31b3da8a
joshjennings98 <josh.jennings@arm.com>
2025-05-30 11:10:43 +0100

    :sparkles: `logs` Add support for reading line by line in FIFO reader

--- utils/logs/fifo_logger.go
+++ utils/logs/fifo_logger.go
@@ -2,10 +2,13 @@ package logs
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"sync"
+
+	"github.com/ARM-software/golang-utils/utils/parallelisation"
 )
 
 type FIFOWriter struct {
@@ -39,6 +42,67 @@ func (w *FIFOWriter) Read() string {
 	return string(bytes)
 }
 
+func (w *FIFOWriter) ReadLines(ctx context.Context, delim byte) func(yield func(string) bool) {
+	return func(yield func(string) bool) {
+		var partial []byte
+		for {
+			if err := parallelisation.DetermineContextError(ctx); err != nil {
+				return
+			}
+
+			buf := func() []byte {
+				w.mu.Lock()
+				defer w.mu.Unlock()
+				defer w.Logs.Reset()
+				tmp := w.Logs.Bytes()
+				buf := make([]byte, len(tmp))
+				copy(buf, tmp)
+				return buf
+			}()
+
+			if len(buf) == 0 {
+				if err := parallelisation.DetermineContextError(ctx); err != nil {
+					if len(partial) > 0 {
+						yield(string(partial))
+					}
+					return
+				}
+
+				continue
+			}
+
+			if len(partial) > 0 {
+				buf = append(partial, buf...)
+				partial = nil
+			}
+
+			for {
+				idx := bytes.IndexByte(buf, delim)
+				if idx < 0 {
+					break
+				}
+				line := buf[:idx]
+
+				if len(line) > 0 && line[len(line)-1] == '\r' {
+					line = line[:len(line)-1]
+				}
+				buf = buf[idx+1:]
+				if len(line) == 0 {
+					continue
+				}
+
+				if !yield(string(line)) {
+					return
+				}
+			}
+
+			if len(buf) > 0 {
+				partial = buf
+			}
+		}
+	}
+}
+
 type FIFOLoggers struct {
 	GenericLoggers
 	LogWriter FIFOWriter
@@ -50,6 +114,10 @@ func (l *FIFOLoggers) Check() error {
 
 func (l *FIFOLoggers) Read() string {
 	return l.LogWriter.Read()
+}
+
+func (l *FIFOLoggers) ReadLines(ctx context.Context, delim byte) func(yield func(string) bool) {
+	return l.LogWriter.ReadLines(ctx, delim)
 }
 
 // Close closes the logger
