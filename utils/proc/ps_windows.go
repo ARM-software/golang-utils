aa4f14d268c5e1ffaf81253459e9ef3987dc5180
joshjennings98 <josh.jennings@arm.com>
2025-06-09 11:16:27 +0100

    remove random newline

--- utils/proc/ps_windows.go
+++ utils/proc/ps_windows.go
@@ -1,8 +1,6 @@
 //go:build windows
 // +build windows
 
-//
-
 /*
  * Copyright (C) 2020-2024 Arm Limited or its affiliates and Contributors. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
