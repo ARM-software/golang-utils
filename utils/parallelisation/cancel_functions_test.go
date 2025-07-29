117138bd36157007082a1cc451df147ef5eae53f
Josh <josh.jennings@arm.com>
2025-07-29 10:02:03 +0000

    :bug: Make sure that an improperly initialised cancel store doesn't cause a panic (#657)
    <!--
    Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors.
    All rights reserved.
    SPDX-License-Identifier: Apache-2.0
    -->
    ### Description
    
    <!--
    Please add any detail or context that would be useful to a reviewer.
    -->
    
    Make sure that an improperly initialised cancel store doesn't cause a
    panic
    
    ### Test Coverage
    
    <!--
    Please put an `x` in the correct box e.g. `[x]` to indicate the testing
    coverage of this change.
    -->
    
    - [x]  This change is covered by existing or additional automated tests.
    - [ ] Manual testing has been performed (and evidence provided) as
    automated testing was not feasible.
    - [ ] Additional tests are not required for this change (e.g.
    documentation update).

--- utils/parallelisation/cancel_functions_test.go
+++ utils/parallelisation/cancel_functions_test.go
@@ -5,33 +5,62 @@
 package parallelisation
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/ARM-software/golang-utils/utils/commonerrors"
+	"github.com/ARM-software/golang-utils/utils/commonerrors/errortest"
 )
 
 // Given a CancelFunctionsStore
 // Functions can be registered
 // and all functions will be called
 func TestCancelFunctionStore(t *testing.T) {
-	// Set up some fake CancelFuncs to make sure they are called
-	called1 := false
-	called2 := false
-	cancelFunc1 := func() {
-		called1 = true
-	}
-	cancelFunc2 := func() {
-		called2 = true
-	}
+	t.Run("valid cancel store", func(t *testing.T) {
+		// Set up some fake CancelFuncs to make sure they are called
+		called1 := false
+		called2 := false
+		cancelFunc1 := func() {
+			called1 = true
+		}
+		cancelFunc2 := func() {
+			called2 = true
+		}
 
-	store := NewCancelFunctionsStore()
+		store := NewCancelFunctionsStore()
 
-	store.RegisterCancelFunction(cancelFunc1, cancelFunc2)
+		store.RegisterCancelFunction(cancelFunc1, cancelFunc2)
 
-	assert.Equal(t, 2, store.Len())
+		assert.Equal(t, 2, store.Len())
 
-	store.Cancel()
+		store.Cancel()
 
-	assert.True(t, called1)
-	assert.True(t, called2)
+		assert.True(t, called1)
+		assert.True(t, called2)
+	})
+
+	t.Run("incorrectly initialised cancel store", func(t *testing.T) {
+		called1 := false
+		called2 := false
+		cancelFunc1 := func() {
+			called1 = true
+		}
+		cancelFunc2 := func() {
+			called2 = true
+		}
+
+		store := CancelFunctionStore{}
+
+		store.RegisterCancelFunction(cancelFunc1, cancelFunc2)
+
+		assert.Equal(t, 2, store.Len())
+
+		err := store.Execute(context.Background())
+		errortest.AssertError(t, err, commonerrors.ErrUndefined)
+
+		assert.False(t, called1)
+		assert.False(t, called2)
+	})
 }
