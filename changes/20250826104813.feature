ca09cb685116500cf028448cf353ae461c2acb96
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2025-08-26 12:01:28 +0200

    :sparkles: `[safeio]` Add the ability to perform safe concatenations like `cat` (#687)
    <!--
    Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors.
    All rights reserved.
    SPDX-License-Identifier: Apache-2.0
    -->
    ### Description
    - add a way to combine streams like `cat` in a safe manner
    
    
    ### Test Coverage
    
    <!--
    Please put an `x` in the correct box e.g. `[x]` to indicate the testing
    coverage of this change.
    -->
    
    - [x]  This change is covered by existing or additional automated tests.
    - [ ] Manual testing has been performed (and evidence provided) as
    automated testing was not feasible.
    - [ ] Additional tests are not required for this change (e.g.
    documentation update).

---
+++ changes/20250826104813.feature
@@ -0,0 +1,1 @@
+:sparkles: `[safeio]` Add the ability to perform safe concatenations like `cat`
--- utils/safeio/copy.go
+++ utils/safeio/copy.go
@@ -8,14 +8,24 @@ import (
 	"github.com/ARM-software/golang-utils/utils/parallelisation"
 )
 
-// CopyDataWithContext copies from src to dst similarly to io.Copy but with context control to stop when asked to.
+// CopyDataWithContext copies from src to dst similarly to io.Copy but with context control to stop when asked.
 func CopyDataWithContext(ctx context.Context, src io.Reader, dst io.Writer) (copied int64, err error) {
 	return copyDataWithContext(ctx, src, dst, io.Copy)
 }
 
-// CopyNWithContext copies n bytes from src to dst similarly to io.CopyN but with context control to stop when asked to.
+// CopyNWithContext copies n bytes from src to dst similarly to io.CopyN but with context control to stop when asked.
 func CopyNWithContext(ctx context.Context, src io.Reader, dst io.Writer, n int64) (copied int64, err error) {
 	return copyDataWithContext(ctx, src, dst, func(dst io.Writer, src io.Reader) (int64, error) { return io.CopyN(dst, src, n) })
+}
+
+// CatN concatenates n bytes from multiple sources to dst. It is intended to provide functionality quite similar to `cat` posix command but with context control.
+func CatN(ctx context.Context, dst io.Writer, n int64, src ...io.Reader) (copied int64, err error) {
+	return CopyNWithContext(ctx, NewContextualMultipleReader(ctx, src...), dst, n)
+}
+
+// Cat concatenates bytes from multiple sources to dst. It is intended to provide functionality quite similar to `cat` posix command but with context control.
+func Cat(ctx context.Context, dst io.Writer, src ...io.Reader) (copied int64, err error) {
+	return CopyDataWithContext(ctx, NewContextualMultipleReader(ctx, src...), dst)
 }
 
 func copyDataWithContext(ctx context.Context, src io.Reader, dst io.Writer, copyFunc func(io.Writer, io.Reader) (int64, error)) (copied int64, err error) {
