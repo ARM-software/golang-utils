e2a185512fd28869d67a538d73c69670a5a8428e
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2026-06-04 21:02:51 +0100

    :gear: fix CI?

--- utils/serialization/xml.go
+++ utils/serialization/xml.go
@@ -8,7 +8,7 @@ import serializationxml "github.com/ARM-software/golang-utils/utils/serializatio
 
 // UnmarshallXML unmarshals XML bytes into value.
 //
-// This compatibility wrapper preserves the historical serialization package API
+// This compatibility wrapper preserves the historical serialisation package API
 // while delegating the actual implementation to the dedicated xml subproject.
 func UnmarshallXML(data []byte, value any) error {
 	return serializationxml.UnmarshallXML(data, value)
